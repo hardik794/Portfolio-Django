85d6f5edce0fcde1b2e4605a96ea21bfef2c11ae
Hardik Savaliya <hardiksavaliya0794@gmail.com>
2021-05-24 22:42:38 +0530

    commit

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (Images)
 MEDIA_URL = '/media/'
