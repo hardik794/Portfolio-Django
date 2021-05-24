0199b75a9ef568f5ef4a92e33402b0451b190eb2
Hardik Savaliya <hardiksavaliya0794@gmail.com>
2021-05-24 21:22:10 +0530

    portfolio2

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz&^8(*8ibwds!j*_y(3qw_(6mooqge2ajjlr)#j4)kuq2o5=y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
